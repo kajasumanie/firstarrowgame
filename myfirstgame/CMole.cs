2b73d837926242d122ff1ef7379875a6a3e6ebe6
kajasumanie <kkajasu@gmail.com>
2018-01-24 10:01:16 +0530

    my first try to create a game

---
+++ myfirstgame/CMole.cs
@@ -0,0 +1,42 @@
+﻿using myfirstgame.Properties;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myfirstgame
+{
+    class CMole : CImageBase
+    {
+        public CMole()
+            :base(Resources.mole)
+        {
+            _moleHotSpot.X = Left + 20;
+            _moleHotSpot.Y = Top - 1;
+            _moleHotSpot.Width = 30;
+            _moleHotSpot.Height = 40;
+        }
+        private Rectangle _moleHotSpot = new Rectangle();
+        public void Update(int X,int Y)
+        {
+            Left = X;
+            Top = Y;
+            _moleHotSpot.X = Left + 20;
+            _moleHotSpot.Y = Top - 1;
+
+        }
+        public bool Hit(int X,int Y)
+        {
+            Rectangle c = new Rectangle(X, Y, 1,1);
+            if(_moleHotSpot.Contains(c))
+            {
+                return true;
+
+            }
+            return false;
+           
+        }
+    }
+}
---
+++ myfirstgame/Form1.cs
@@ -0,0 +1,54 @@
+﻿#define My_Debug
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace myfirstgame
+{
+    public partial class Form1 : Form
+    {
+#if My_Debug
+        int _cursX = 0;
+        int _cursY = 0;
+#endif
+        CMole _mole;
+        public Form1()
+        {
+            InitializeComponent();
+            _mole = new CMole() { Left = 100,Top = 200};
+        }
+
+        private void TimerGameStart_Tick(object sender, EventArgs e)
+        {
+
+        }
+       protected override void OnPaint(PaintEventArgs e)
+        {
+            Graphics dc = e.Graphics;
+#if My_Debug
+            TextFormatFlags flags = TextFormatFlags.Left | TextFormatFlags.EndEllipsis;
+            Font _font = new System.Drawing.Font("Stencil", 12, FontStyle.Regular);
+            TextRenderer.DrawText(dc, "x=" + _cursX.ToString() + ":" + "Y=" + _cursY.ToString(), _font,
+                new Rectangle(100, 28, 120, 20), SystemColors.ControlText, flags);
+#endif
+            _mole.DrawImage(dc);
+            base.OnPaint(e);
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+#if My_Debug
+            _cursX = e.X;
+            _cursY = e.Y;
+        
+#endif
+            this.Refresh();
+        }
+    }
+}
